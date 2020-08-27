d960adec824f021cf61b120d460bb4993760b53d
Dino Chang <dino.chang@104.com.tw>
2020-08-27 10:56:21 +0800

    [下樓梯] 初始

---
+++ downStair/downStair.js
@@ -0,0 +1,16 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 600,
+  height: 800,
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {}
+function create() {}
+function update() {}
