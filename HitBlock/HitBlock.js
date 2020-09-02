259b91e4018b98259a4c345107317c7102887f73
Dino Chang <dino.chang@104.com.tw>
2020-09-02 15:53:49 +0800

    [打磚塊] 17 物理碰撞與控制

--- HitBlock/HitBlock.js
+++ HitBlock/HitBlock.js
@@ -1,3 +1,4 @@
+let speed = 160;
 const blockColorMap = [
   "blueBlock",
   "greenBlock",
@@ -36,6 +37,10 @@ const stage = new Phaser.Class({
       add: true,
     });
     board = this.physics.add.image(400, 550, "board").setScale(0.5, 0.3);
+    // 板子碰撞後不被移動
+    board.body.immovable = true;
+    // 不要讓板子離開我們的世界
+    board.setCollideWorldBounds(true);
     blocks = this.physics.add.group();
     for (let i = 0; i < 200; i++) {
       blocks.create(
@@ -50,9 +55,42 @@ const stage = new Phaser.Class({
     });
 
     ball = this.physics.add.sprite(400, 500, "ball").setScale(0.7);
+    //碰撞係數
+    ball.setBounce(1);
+    //碰撞體改變成圓形
+    ball.body.setCircle(10);
+    //初始速度
+    ball.setVelocityY(200);
+    // 世界碰撞
+    ball.setCollideWorldBounds(true);
+    // 不與下方碰撞
+    this.physics.world.checkCollision.down = false;
+    this.physics.add.collider(ball, blocks, blockBounce, null, this);
+    this.physics.add.collider(ball, board, boardBounce, null, this);
+
+    //加上鍵盤控制物件
+    cursors = this.input.keyboard.createCursorKeys();
   },
-  update: function () {},
+  update: function () {
+    if (cursors.left.isDown) {
+      board.setVelocityX(speed * -1);
+    } else if (cursors.right.isDown) {
+      board.setVelocityX(speed);
+    } else {
+      board.setVelocityX(0);
+    }
+  },
 });
+
+function blockBounce(ball, block) {
+  block.disableBody(true, true);
+}
+
+function boardBounce(ball, board) {
+  // 透過計算看他打在板子的哪一邊給他一個向左或向右的速度
+  ball.body.setVelocityX((190 * (ball.x - board.x)) / board.width);
+}
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
